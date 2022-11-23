b29f1051892b1c1a44b920ae048d03f069fe6261
det maneh <detmaneh@dets-MacBook-Pro.local>
2022-11-22 18:16:34 -0600

    fixing navbar display to compress while on phone size screen

--- src/components/PageHeader/PageHeader.js
+++ src/components/PageHeader/PageHeader.js
@@ -1,36 +1,58 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import svgLogo from '../../assets/acme-1.svg';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import AppContext from '../AppContext/AppContext';
 import { Link } from 'react-router-dom'
+import { NavItem } from 'react-bootstrap';
 
 const PageHeader = (props) => {
   const myContext = useContext(AppContext);
   const inComponents = props.components;
-  const myLogoStyle = { resizeMode: "contain",alignItems: "center",width: '100%', height: '100%' };
+  const myLogoStyle = { resizeMode: "contain",alignItems: "center",width: '100%'};
+  const questionStyle = {borderRadius:"15px", textAlign:"center"};
+  const linkStyle = { textDecoration: 'none', color:'#808080',display: 'inline-block'};
+  const containCenterImage = {display: "flex",justifyContent: "center", alignItems: "center"}
+  let navBarObjectToggle = document.getElementById('navbarContentToggle');
   const setClickedLinkIndex = (index) => {
     myContext.setIndexValue(index);
+    toggleNav();
   }
+
+  const toggleNav = () => {
+    navBarObjectToggle.click();
+  }
+
+  const addSpace = (word) => {
+    return word.replace(/([A-Z])/g, ' $1').trim();
+  }
+
+  const builtLinkFromComp= (comp) => {
+    return (
+      <Link to={comp.path} id={comp.name} 
+        key = {comp.index} style={linkStyle}>
+        {addSpace(comp.name)}
+      </Link>
+    );
+  }
+
+  useEffect(() => {
+    navBarObjectToggle = document.getElementById('navbarContentToggle');
+  }, []);
+
   return (
-    <Navbar bg="light" expand={false} data-testid="PageHeader">
-      
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="text-center" defaultActiveKey="/">
+    <Navbar justify expand={false}>
+      <Nav bg="light" data-testid="PageHeader" style={{display:"contents"}}>
+      <Navbar.Toggle id="navbarContentToggle" style={{ display:"inline-block", width:"25%"}}/>
+      <Navbar.Collapse id="navbarContent">
+        <Nav className="text-center" defaultActiveKey="/" >
           {(() => {
             let components = [];
             inComponents.map((component, i) => { 
               components.push(
-                <Link 
-                  to={component.path} 
-                  id={component.name} 
-                  key = {i}
-                  onClick={ event =>  setClickedLinkIndex(component.index)}
-                  style={{ textDecoration: 'none', color:'#808080'}}
-                >
-                  {component.name.replace(/([A-Z])/g, ' $1').trim()}
-                </Link>
+                <NavItem key = {i} onClick={ () => { setClickedLinkIndex(component.index) }}>
+                    {builtLinkFromComp(component)}
+                </NavItem>
               )
               return true;
             })
@@ -38,14 +60,21 @@ const PageHeader = (props) => {
           })()}
         </Nav>
       </Navbar.Collapse>
-      <Navbar.Brand>
-        <img src={svgLogo} style={myLogoStyle} />
-      </Navbar.Brand>
-      <Navbar.Text style={{height:"10%", width:"10%"}}>
-        <div style={{background:"indigo", color:"white", height:"25%", width:"25%", fontWeight:"bold",borderRadius:"15px", textAlign:"center"}}>
-          ?
+      <Nav style={{width:"25%", display:"inline-block", width:"25%"}}>
+        <Navbar.Brand>
+            <Link to={inComponents[0].path} id={inComponents[0].name} key = {inComponents[0].index}
+              onClick={ event =>  setClickedLinkIndex(inComponents[0].index)}
+              style={linkStyle} >
+                <div style={containCenterImage}><img src={svgLogo} style={myLogoStyle}/></div>
+            </Link>
+        </Navbar.Brand>
+      </Nav>
+      <Navbar.Text style={{height:"10%", width:"10%",textAlign:"center", display:"inline-block", width:"25%"}}>
+        <div style={questionStyle}>
+          <span style={{background:"indigo", color:"white", fontWeight:"bold",borderRadius:"100%", padding:"5%",textAlign:"center"}}>?</span>
         </div>
       </Navbar.Text>
+      </Nav>
     </Navbar>
   );
 }
